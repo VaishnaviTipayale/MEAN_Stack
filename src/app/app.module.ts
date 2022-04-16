0a5f69942e00ef40df2a01fe905c724e4bc61371
“vaishnavi” <vaishnavitipayale2017@gmail.com>
2022-04-15 20:27:01 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VaishnaviComponent } from './vaishnavi/vaishnavi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VaishnaviComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/vaishnavi/vaishnavi.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VaishnaviComponent } from './vaishnavi.component';
+
+describe('VaishnaviComponent', () => {
+  let component: VaishnaviComponent;
+  let fixture: ComponentFixture<VaishnaviComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VaishnaviComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VaishnaviComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
